29ee533eb54e1b833abcf76e51c4f22321d520f3
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-09-07 21:40:32 +0900

    feat: add logo image

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import logo from 'assets/images/logo-primary-white@2X.png';
 import './Header.scss';
 
 const Header = () => {
-  return <div className='header'></div>;
+  return (
+    <div className='header'>
+      <Image className='logo' src={logo} />
+    </div>
+  );
 };
 
 export default Header;
