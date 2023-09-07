31e71d3bcd8ccea7edef2429c6ed88d44d43e8bb
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-09-07 23:18:44 +0900

    feat: update styles of header & sidebar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import logo from 'assets/images/logo-primary-white@2X.png';
 import './Header.scss';
 
@@ -7,6 +9,9 @@ const Header = () => {
   return (
     <div className='header'>
       <Image className='logo' src={logo} />
+      <div className='toggle'>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
     </div>
   );
 };
