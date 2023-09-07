72335895d22ed708052154cff372f1ee16978b10
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-09-07 22:39:14 +0900

    feat: update sidebar layout

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,27 +1,29 @@
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenNib } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import './Sidebar.scss';
 
 const SpinSide = () => {
   return (
-    <div style={{ display: 'flex', height: '100%' }}>
-      <Sidebar>
-        <Menu>
-          <SubMenu label='Charts'>
-            <MenuItem> Pie charts</MenuItem>
-            <MenuItem> Line charts</MenuItem>
-            <MenuItem> Bar charts</MenuItem>
-          </SubMenu>
-          <SubMenu label='Maps'>
-            <MenuItem> Google maps</MenuItem>
-            <MenuItem> Open street maps</MenuItem>
-          </SubMenu>
-          <SubMenu label='Theme'>
-            <MenuItem> Dark</MenuItem>
-            <MenuItem> Light</MenuItem>
-          </SubMenu>
-        </Menu>
-      </Sidebar>
-    </div>
+    <Sidebar className='sidebar'>
+      <Menu>
+        <SubMenu icon={<FontAwesomeIcon icon={faPenNib} />} label='Charts'>
+          <MenuItem> Pie charts</MenuItem>
+          <MenuItem> Line charts</MenuItem>
+          <MenuItem> Bar charts</MenuItem>
+        </SubMenu>
+        <SubMenu icon={<FontAwesomeIcon icon={faPenNib} />} label='Maps'>
+          <MenuItem> Google maps</MenuItem>
+          <MenuItem> Open street maps</MenuItem>
+        </SubMenu>
+        <SubMenu suffix='🔥' icon={<FontAwesomeIcon icon={faEnvelope} />} label='Theme'>
+          <MenuItem> Dark</MenuItem>
+          <MenuItem> Light</MenuItem>
+        </SubMenu>
+      </Menu>
+    </Sidebar>
   );
 };
 
