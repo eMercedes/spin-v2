ac647ce995f613352cc9b1b79c52b101cf45db50
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-09-11 17:59:02 +0900

    fix: update cash card

--- src/components/cards/cash/index.tsx
+++ src/components/cards/cash/index.tsx
@@ -1,6 +1,7 @@
 import css from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
+import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import './Cash.scss'
 
 interface CashcardPropTypes {
@@ -11,6 +12,14 @@ interface CashcardPropTypes {
     status?: 'up' | 'down' | 'zero'
 }
 
+interface ICONProps {
+    [icon: string]: IconProp
+}
+const ICONS: ICONProps = {
+    up: faCaretUp,
+    down: faCaretDown,
+}
+
 const CashCard = ({ className, title, total, change = 0, status = 'up' }: CashcardPropTypes) => (
     <div className={css('cash-card', className)}>
         <div className='title'>{title}</div>
@@ -18,7 +27,7 @@ const CashCard = ({ className, title, total, change = 0, status = 'up' }: Cashca
         <div className='change'>
             {change ? (
                 <>
-                    <FontAwesomeIcon className={css(status, 'me-2')} icon={faCaretUp} />$ {(Math.round(change * 100) / 100).toFixed(2)}
+                    <FontAwesomeIcon className={css(status, 'me-2')} icon={ICONS[status]} />$ {(Math.round(change * 100) / 100).toFixed(2)}
                 </>
             ) : (
                 'No Change'
