2cb24242472f4ddac37036ebf6816f8b4bad9279
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-09-11 09:55:31 +0900

    feat: add outlet route flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Root from 'containers/root'
 import Financial from 'containers/financial'
-import configureStore from './store'
 import './App.scss'
-
-const { store, persistor } = configureStore()
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route>
-            <Route path='/' element={<Root />} />
-            <Route path='/start'>
-                <Route path='/start/financial' element={<Financial />} />
+        <Route path='/' element={<Root />}>
+            <Route path='start'>
+                <Route path='financial' element={<Financial />} />
             </Route>
         </Route>,
     ),
 )
 
 function App() {
-    return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <RouterProvider router={router} />
-            </PersistGate>
-        </Provider>
-    )
+    return <RouterProvider router={router} />
 }
 
 export default App
--- src/containers/root/index.tsx
+++ src/containers/root/index.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import { Header, Sidebar } from 'components'
 
 function Root() {
@@ -6,6 +7,7 @@ function Root() {
             <Header />
             <div className='content'>
                 <Sidebar />
+                <Outlet />
             </div>
         </div>
     )
