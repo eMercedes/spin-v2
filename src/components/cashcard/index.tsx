b40638fc16a5ea94329a26d200abb0f96c352df3
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-09-11 10:56:59 +0900

    fix: update padding

--- src/components/cashcard/index.tsx
+++ src/components/cashcard/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
 import css from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
 import './Cashcard.scss'
 
 interface CashcardPropTypes {
+    className?: string
     title: string
     total: number
     change?: number
     status?: 'up' | 'down' | 'zero'
 }
 
-const CashCard = ({ title, total, change = 0, status = 'up' }: CashcardPropTypes) => (
-    <div className='cash-card'>
+const CashCard = ({ className, title, total, change = 0, status = 'up' }: CashcardPropTypes) => (
+    <div className={css('cash-card', className)}>
         <div className='title'>{title}</div>
         <div className='amount'>$ {total}</div>
         <div className='change'>
--- src/containers/financial/index.tsx
+++ src/containers/financial/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Cashcard from 'components/cashcard'
 import './Financial.scss'
 
@@ -7,9 +6,9 @@ function Financial() {
         <div className='financial'>
             <div className='status'>
                 <Cashcard title='Main Fundings' total={694} change={30} />
-                <Cashcard title='Invoices' total={254} change={828} status='down' />
-                <Cashcard title='Accounts Receivable' total={346} change={663} />
-                <Cashcard title='Secure Holdings' total={7} status='zero' />
+                <Cashcard className='ms-3' title='Invoices' total={254} change={828} status='down' />
+                <Cashcard className='ms-3' title='Accounts Receivable' total={346} change={663} />
+                <Cashcard className='ms-3' title='Secure Holdings' total={7} status='zero' />
             </div>
         </div>
     )
